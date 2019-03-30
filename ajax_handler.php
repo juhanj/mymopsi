ce15ba08f725e8b693743799f1c95c1770caee83
Juhani Järviö <juhani.jarvio@gmail.com>
2019-03-30 13:01:51 +0200

    Fixed sending JSON-request to server.
    New sendJSON function to replace ajax() one. Uses async/await, instead of promises (.then()-functions). No need for callback function as parameter.
    
    The url-parameter was supposed to be absolute path, but don't know how to do that, so it's relative. Eh, good enough, I guess.
    
    ajax_handler.php : Fixed receiving JSON-request. It can now do that. Plus some header stuff for security.

--- ajax_handler.php
+++ ajax_handler.php
@@ -1,7 +1,11 @@
 <?php declare(strict_types=1);
 
-if ( empty($_GET['req']) AND empty($_POST['req']) ) {
-	header('400 Bad Request');
+$req = $_GET
+	?: $_POST
+	?: json_decode( file_get_contents('php://input'), true );
+
+if ( empty($req) ) {
+	header('400 Bad Request', true, 400);
 	exit;
 }
 
@@ -10,11 +14,21 @@ require './components/_start.php';
  * @var $db DBConnection
  */
 
-$req = $_GET ?: $_POST;
 $collection = new Collection( $db, $req );
 
-$result = $collection->result;
+$result = [
+	'request' => $req,
+	'result' => $collection->result
+];
 
-header('Content-Type: application/json');
+/*
+ * Return result in JSON format back to client.
+ */
 echo json_encode( $result );
+
+header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Credentials: true" );
+header('Content-Type: application/json');
 exit();
--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 "use strict";
-function ajax( data, callback ) {
-    fetch( 'ajax_handler.php', {
+//TODO: Change url-paramtere to absolute path. Possible even? --jj190330
+async function sendJSON ( data, url = './ajax_handler.php' ) {
+    console.log("sendJSON(): Sending request");
+    let response = await fetch( url, {
         method: 'post',
         credentials: 'same-origin',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify( data )
-    })
-        .then(function(response) {
-            return response.json();
-        })
-        .then(
-            callback
-        );
+    });
+    return await response.json();
+}
+
+//TODO: Change url-paramtere to absolute path. Possible even? --jj190330
+async function sendFormData ( data, url = './ajax_handler.php' ) {
+    console.log("sendFormData(): Sending Fetch FormData POST request...");
+    let response = await fetch( url, {
+        method: 'post',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: data
+    });
+    return await response.json();
 }
