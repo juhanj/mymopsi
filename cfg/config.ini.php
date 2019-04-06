dd574a2962e8845bbb3499718e6fcc709e20064a
Juhani Järviö <juhani.jarvio@gmail.com>
2019-04-07 01:02:02 +0300

    Update config.ini.php
    Some comments added.
    Added path to config (see comment).

--- cfg/config.ini.php
+++ cfg/config.ini.php
@@ -9,12 +9,22 @@ user = "root"
 pass = ""
 
 [Admin]
+; Used for admin access to all collections
+; Hashed/salted password here
 password = ""
 
 [Misc]
+; Localhost / server difference on Perl setup
 perl = "./path/to/perl"
-path_to_collections = "./path/to/collections/"
-web_root_path = "/web/root/path/to/files/"
+; Where are the images saved on the server.
+path_to_collections = "./collections/"
+; //TODO: comment --jj190406
+web_root_path = "/mopsi_dev/mymopsi/"
+
+[Config]
+; The config-file on server is not located on web-root
+; Different location for localhost and online-server
+config = "./cfg/config.ini"
 
 [Testing]
 perl = "path/to/perl"
