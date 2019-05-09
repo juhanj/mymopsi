ac97062225e1b460fea8e19a6d222c7d28ff6039
Juhani Järviö <juhani.jarvio@gmail.com>
2019-05-09 23:32:10 +0300

    Update index.js
    Planning for future features

---
+++ js/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * @param {int} id
+ */
+function removeCollectionFromCookies ( id ) {}
+
+let localCollectionsDiv = document.getElementById('local-collections');
+let publicCollectionsDiv = document.getElementById('public-collections');
+
+//TODO:
+// Remove collection from local cookies
+// Attach listener to a button next to list item
+// function
+
+//TODO:
+// Get local collections from cookies
+// geCookies ( collList )
+// print to localCollectionsDiv
+
+//TODO:
+// Fetch public collections from the server
+// sendJSON ( getPublicCollections )
+// print to publicCollectionsDiv
