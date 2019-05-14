014999dc76e8153f30fde6477c895edde7a87196
Juhani Järviö <juhani.jarvio@gmail.com>
2019-05-14 12:09:42 +0300

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -2,6 +2,3 @@
 require	$_SERVER['DOCUMENT_ROOT'] . '/mopsi_dev/mymopsi/components/_start.php';
 /*/////////////////////////////////////////////////*/
 
-
-debug( $_SERVER["DOCUMENT_ROOT"] );
-debug( __DIR__ );
