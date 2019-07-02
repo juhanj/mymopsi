292c7f96a34184195abaa0810ca3e28d0002267e
Juhani Järviö <juhani.jarvio@gmail.com>
2019-07-02 17:55:11 +0300

    Added bunch of MyMopsi

--- components/html-footer.php
+++ components/html-footer.php
@@ -1,9 +1,14 @@
 <?php declare(strict_types=1); ?>
 
 <footer class="site-footer margins-off" id="site-footer">
-	<div class="left"></div>
-	<div class="center"></div>
+	<div class="left">
+		<p>MyMopsi</p>
+	</div>
+	<div class="center">
+		<h1 class="footer-title"><?= $lang->HEADER_TITLE ?></h1>
+	</div>
 	<div class="right">
+		<span>MyMopsi</span>
         <!--
             //TODO settings dropdown here? -jj190511
             For language selecting, and info page.
--- components/html-header.php
+++ components/html-header.php
@@ -1,9 +1,13 @@
 <?php declare(strict_types=1); ?>
 
 <header class="site-header margins-off" id="site-header">
-	<div class="left"></div>
+	<div class="left">
+		<h1>MyMopsi</h1>
+	</div>
 	<div class="center">
 		<h1 class="page-title"><?= $lang->HEADER_TITLE ?></h1>
 	</div>
-	<div class="right"></div>
+	<div class="right">
+		<span>MyMopsi</span>
+	</div>
 </header>
