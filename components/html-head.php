836c6c65c109dd24771dfc3ebcb035f7d5490f44
Juhani Järviö <juhani.jarvio@gmail.com>
2019-07-02 17:54:43 +0300

    Update html-head.php
    Polyfill for `<dialog>`
    
    Day.js library for handling dates.

--- components/html-head.php
+++ components/html-head.php
@@ -19,6 +19,13 @@
 	<!-- Page specific CSS file -->
 	<link rel="stylesheet" href="<?= WEB_PATH ?>/css/<?= CURRENT_PAGE ?>.css">
 
+	<!-- Polyfill for <dialog> -element -->
+	<script defer src="https://cdnjs.cloudflare.com/ajax/libs/dialog-polyfill/0.5.0/dialog-polyfill.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dialog-polyfill/0.5.0/dialog-polyfill.min.css" />
+
+	<!-- Day.js library for handling datetimes in javascript -->
+	<script defer src="https://unpkg.com/dayjs"></script>
+
 	<!-- Main javascript file -->
 	<script defer src="<?= WEB_PATH ?>/js/main.js"></script>
 	<!-- Page specific javascript file -->
