01d076544744d2a0a5c0af086c47d645ca30a195
Juhani Järviö <juhani.jarvio@gmail.com>
2019-07-07 22:19:23 +0300

    Footer updated
    See previous update to header. Same reasoning. Too complicated, simplified.

--- components/html-footer.php
+++ components/html-footer.php
@@ -1,18 +1,12 @@
 <?php declare(strict_types=1); ?>
 
 <footer class="site-footer margins-off" id="site-footer">
-	<div class="left">
+	<section class="left">
 		<p>MyMopsi</p>
-	</div>
-	<div class="center">
-		<h1 class="footer-title"><?= $lang->HEADER_TITLE ?></h1>
-	</div>
-	<div class="right">
-		<span>MyMopsi</span>
-        <!--
-            //TODO settings dropdown here? -jj190511
-            For language selecting, and info page.
-            Without bootstrap gonna be a challenge, but eh I'm sure it'll be fine.
-        -->
-    </div>
+	</section>
+	<section class="right">
+		<a href="<?= WEB_PATH ?>/settings.php" class="button settings-link">
+			<i class="material-icons">settings</i>
+		</a>
+    </section>
 </footer>
