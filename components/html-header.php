a14a1716a454e08a4fe73d6d6062f5399253a013
Juhani Järviö <juhani.jarvio@gmail.com>
2019-07-07 22:18:49 +0300

    Header updated
    The header structure was too complicated, so now it only contains the page title. Adding new sections if needed is not too complicated, but this makes code much more maintainable.

--- components/html-header.php
+++ components/html-header.php
@@ -1,13 +1,5 @@
 <?php declare(strict_types=1); ?>
 
 <header class="site-header margins-off" id="site-header">
-	<div class="left">
-		<h1>MyMopsi</h1>
-	</div>
-	<div class="center">
-		<h1 class="page-title"><?= $lang->HEADER_TITLE ?></h1>
-	</div>
-	<div class="right">
-		<span>MyMopsi</span>
-	</div>
+	<h1 class="page-title"><?= $lang->HEADER_TITLE ?></h1>
 </header>
