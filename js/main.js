68f0fe402fd36f685a28cb6e0211626312262d2d
Juhani Järviö <juhani.jarvio@gmail.com>
2019-10-04 14:29:53 +0300

    Update main.js
    Formatting. Removed localCookies function. Added returnJSON param to fetch funtions.

--- js/main.js
+++ js/main.js
@@ -5,18 +5,19 @@
  * Usage: sendJSON(params).then((jsonResponse) => {});
  * @param data Changed to JSON before sending
  * @param {string} url optional, default == ./ajax-handler.php
+ * @param {boolean} returnJSON
  * @returns {Promise<object>} JSON
  */
-async function sendJSON(data, url = './ajax-handler.php') {
-	let response = await fetch(url, {
+async function sendJSON ( data, url = './ajax-handler.php', returnJSON = true ) {
+	let response = await fetch( url, {
 		method: 'post',
 		credentials: 'same-origin',
 		headers: {
 			'Content-Type': 'application/json'
 		},
-		body: JSON.stringify(data)
-	});
-	return await response.json();
+		body: JSON.stringify( data )
+	} );
+	return (returnJSON) ? await response.json() : await response;
 }
 
 /**
@@ -24,16 +25,17 @@ async function sendJSON(data, url = './ajax-handler.php') {
  * Usage: sendForm(formdata).then((jsonResponse) => {});
  * @param {FormData} data Form-element, must be an FormData object
  * @param {string} url optional, default == ./ajax-handler.php
+ * @param {boolean} returnJSON
  * @returns {Promise<object>} JSON
  */
-async function sendForm(data, url = './ajax-handler.php') {
-	let response = await fetch(url, {
+async function sendForm ( data, url = './ajax-handler.php', returnJSON = true ) {
+	let response = await fetch( url, {
 		method: 'post',
 		credentials: 'same-origin',
 		// explicitly no Content-Type with FormData
 		body: data
-	});
-	return await response.json();
+	} );
+	return (returnJSON) ? await response.json() : await response;
 }
 
 /**
@@ -42,9 +44,9 @@ async function sendForm(data, url = './ajax-handler.php') {
  * @param {string} [value='']
  * @param {int} [days=30] How long will the browser store the cookie, in days.
  */
-function setCookie(name, value = '', days = 30) {
+function setCookie ( name, value = '', days = 30 ) {
 	let date = new Date();
-	date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+	date.setTime( date.getTime() + (days * 24 * 60 * 60 * 1000) );
 	let expires = "; expires=" + date.toUTCString();
 
 	document.cookie = name + "=" + (value || "") + expires + "; path=/mopsi_dev/mymopsi";
@@ -54,17 +56,17 @@ function setCookie(name, value = '', days = 30) {
  * @param {string} name
  * @returns {string|null}
  */
-function getCookie(name) {
+function getCookie ( name ) {
 	let nameEQ = name + "=";
-	let cookies_array = document.cookie.split(';');
+	let cookies_array = document.cookie.split( ';' );
 	let cookie, i;
-	for (i = 0; i < cookies_array.length; i++) {
+	for ( i = 0; i < cookies_array.length; i++ ) {
 		cookie = cookies_array[i];
-		while (cookie.charAt(0) === ' ') {
-			cookie = cookie.substring(1, cookie.length);
+		while ( cookie.charAt( 0 ) === ' ' ) {
+			cookie = cookie.substring( 1, cookie.length );
 		}
-		if (cookie.indexOf(nameEQ) === 0) {
-			return cookie.substring(nameEQ.length, cookie.length);
+		if ( cookie.indexOf( nameEQ ) === 0 ) {
+			return cookie.substring( nameEQ.length, cookie.length );
 		}
 	}
 	return null;
@@ -74,35 +76,8 @@ function getCookie(name) {
  * Delete the cookie by setting max-age to -1.
  * @param {string} name
  */
-function deleteCookie(name) {
+function deleteCookie ( name ) {
 	document.cookie = name + '=; Max-Age=-1;';
-}
-
-/**
- * Add a collection to the local list of collection kept in the cookies.
- * Doesn't allow duplicates (does not add a collection if it already found in the array)
- * @param {string} name
- * @param {string} uid
- */
-function addCollectionToCookies(name, uid) {
-	let collections = JSON.parse(getCookie('collections'));
-
-	if ( !collections ) {
-		collections = [];
-	}
-
-	let duplicateFound = false;
-	collections.forEach( (item) => {
-		duplicateFound = ( item.name === name && item.uid === uid )
-			? true
-			: duplicateFound;
-	});
-
-	if ( !duplicateFound ) {
-		collections.push({name, uid});
-
-		setCookie('collections', JSON.stringify(collections));
-	}
 }
 
 /**
@@ -116,7 +91,7 @@ const KB = 1024;
  */
 const MB = 1048576;
 /**
- * Gigabyte, MB * MB (Probably won't need this one)
+ * Gigabyte, MB * MB (Probably (hopefully?) won't need this one)
  * @type {number} 1 073 741 824
  */
 const GB = 1073741824;
