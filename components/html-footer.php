bec214064da09d4bf53c82fcd40e05bc568459f1
Juhani Järviö <juhani.jarvio@gmail.com>
2019-10-21 10:27:59 +0300

    Update html-footer.php
    Whitespace

--- components/html-footer.php
+++ components/html-footer.php
@@ -1,12 +1,15 @@
 <?php declare(strict_types=1); ?>
 
 <footer class="site-footer margins-off" id="site-footer">
+
 	<section class="left">
 		<p>MyMopsi</p>
 	</section>
+
 	<section class="right">
 		<a href="<?= WEB_PATH ?>/settings.php" class="button light settings-link">
 			<i class="material-icons">settings</i>
 		</a>
     </section>
+
 </footer>
