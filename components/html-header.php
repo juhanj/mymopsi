e1958e081db801e6deb681e949324e4d4f191625
Juhani Järviö <juhani.jarvio@gmail.com>
2020-02-06 01:12:20 +0200

    Update html-header.php
    whitespace

--- components/html-header.php
+++ components/html-header.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1); ?>
 
 <header class="site-header margins-off" id="site-header">
-	<h1 class="page-title"><?= $lang->HEADER_TITLE ?></h1>
+	<h1 class="page-title">
+		<?= $lang->HEADER_TITLE ?>
+	</h1>
 </header>
