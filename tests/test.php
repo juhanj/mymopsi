c0973e1fb330727ea7dda21a410bc46b4a66fdc9
Juhani Järviö <juhani.jarvio@gmail.com>
2020-05-31 13:06:13 +0300

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -1,7 +1,3 @@
 <?php declare(strict_types=1);
 require $_SERVER['DOCUMENT_ROOT'] . '/mopsi_dev/mymopsi/components/_start.php';
 /*/////////////////////////////////////////////////*/
-
-echo get_include_path();
-
-echo file_get_contents("img/settings.svg",true);
