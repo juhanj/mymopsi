3f06aa1cc4327bdcc4d706357bd90b98dc2b657e
Juhani Järviö <juhani.jarvio@gmail.com>
2021-01-08 13:51:15 +0200

    Update html-footer.php
    Remove MyMopsi text. change settings icon to Google Material icon.

--- components/html-footer.php
+++ components/html-footer.php
@@ -3,14 +3,13 @@
 <footer class="site-footer margins-off" id="site-footer">
 
 	<section class="left">
-		<p>MyMopsi</p>
 	</section>
 
 	<?php // TODO: Fix this on the front page, looks wrong ?>
 	<section class="right">
 		<a href="<?= WEB_PATH ?>/settings.php" class="button light settings-link">
 			Settings
-			<?php echo file_get_contents("img/settings.svg",true); ?>
+			<span class="material-icons">settings</span>
 		</a>
     </section>
 
