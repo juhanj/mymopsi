b6d08c56f554d97f1be3d5a0a97c1ac391598b64
Juhani Järviö <juhani.jarvio@gmail.com>
2021-01-13 11:53:53 +0200

    Update settings.js
    Reload page when language changed

--- js/settings.js
+++ js/settings.js
@@ -6,13 +6,11 @@ document.querySelectorAll("input[name='lang']").forEach((input) => {
 	// Add a listener for user made changes
 	// Changes lang
 	input.addEventListener('click', (element) => {
-		console.log( element.target.value );
 		Cookies.setCookie(
 			'mymopsi_lang',
 			element.target.value,
 			999
 		);
+		location.reload();
 	});
 });
-
-// TODO: Print something to user when language changed. "Refresh page to see changes."
