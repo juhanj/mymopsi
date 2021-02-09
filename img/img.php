3c38f900a93e2bfde32f304fa916a1c887cf969d
Juhani Järviö <juhani.jarvio@gmail.com>
2021-02-09 14:15:30 +0200

    Update img.php
    Can now return random image from collection, as representative of collection. First / last image coming soon, as well as option to choose.
    
    Cache is still not working, nor do I think I'll get it working. Lost cause, at this moment.

--- img/img.php
+++ img/img.php
@@ -6,34 +6,60 @@ require $_SERVER['DOCUMENT_ROOT'] . '/mopsi_dev/mymopsi/components/_start.php';
  * @var User $user
  */
 
-$image_ruuid = $_GET['id']; // Random unique identifier from the database
-$image_thumbnail = $_GET['thumb'] ?? false; // Any value, even empty, is OK
-
-/**
- * @var Image $image
+/*
+ * GET variables:
  */
-$image = $db->query(
-	'select id, collection_id, mediatype, filepath, size from mymopsi_img where random_uid = ? limit 1',
-	[ $image_ruuid ],
-	false,
-	'Image'
-);
+$thumbnail = $_GET['thumb'] ?? null;
+// Single image GET request
+$image_ruid = $_GET['id'] ?? null; // RUID
+// collection image GET request
+$collection_ruid = $_GET['collection'] ?? null; // RUID
+
+
+if ( $collection_ruid ) {
+	$collection = Collection::fetchCollectionByRUID( $db, $collection_ruid );
+	/**
+	 * @var Image $image
+	 */
+	$image = $db->query(
+		'select id, mediatype, filepath, size from mymopsi_img where collection_id = ? order by rand() limit 1',
+		[ $collection->id ],
+		false,
+		'Image'
+	);
+}
+
+else {
+	/**
+	 * @var Image $image
+	 */
+	$image = $db->query(
+		'select id, mediatype, filepath, size from mymopsi_img where random_uid = ? limit 1',
+		[ $image_ruid ],
+		false,
+		'Image'
+	);
+}
 
 if ( !$image or !file_exists( $image->filepath ) ) {
-	header( 'HTTP/1.0 404 Not Found' );
-	header( "location: {$_SERVER['HTTP_REFERER']}" );
+//	header( 'HTTP/1.1 404 Not Found' );
+	http_response_code(404);
 	exit();
 }
 
+// This is never sent from browser, with PPH-script images,
+//  so cache no work
 // the browser will send a $_SERVER['HTTP_IF_MODIFIED_SINCE'] if it has a cached copy
 if ( isset( $_SERVER['HTTP_IF_MODIFIED_SINCE'] ) ) {
 	// if the browser has a cached version of this image, send 304
-	header( 'Last-Modified: ' . $_SERVER['HTTP_IF_MODIFIED_SINCE'], true, 304 );
+	http_response_code(304);
+//	header( 'Last-Modified: ' . $_SERVER['HTTP_IF_MODIFIED_SINCE'], true, 304 );
 	exit();
 }
 
 header( 'Content-Type: ' . $image->mediatype );
 header( 'Content-Length: ' . $image->size );
-header( "Cache-Control: private, max-age=31536000", true );
+// Cache valid 7 days
+header( "Cache-Control: public,max-age=604800,immutable", true );
 
 readfile( $image->filepath );
