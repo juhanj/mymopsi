f026de725b4abaddf14d1c8c5e2b83148ca4f784
Juhani Järviö <juhani.jarvio@gmail.com>
2021-09-28 13:16:59 +0300

    Update config.ini.php
    Added imagemagick command to ini-file. Version difference between localhost and server, different command. Needed for thumbnail generation.

--- cfg/config.ini.php
+++ cfg/config.ini.php
@@ -19,6 +19,8 @@ coll_descr_max_len = 300
 [Misc]
 ; Localhost / server difference in Perl setup
 perl = "./path/to/perl"
+; Localhost / server differences in ImageMagick setup
+imagemagick = "imagemagick convert"
 ; Where are the images saved on the server.
 ; Thumbnails for images are saved under each collections own directory
 path_to_collections = "./path/to/collections/"
