088f80dd3b009b370e602b2b45cd1542d7a80c77
Juhani Järviö <juhani.jarvio@gmail.com>
2021-10-17 21:42:56 +0300

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -1,9 +1,3 @@
 <?php declare(strict_types=1);
 require '../components/_start.php';
 /*/////////////////////////////////////////////////*/
-
-$file = "C:\Users\Jq\Pictures\Joensuu/IMG_20210606_235347.jpg";
-
-$result = Common::runExiftool( $file );
-
-debug( $result );
