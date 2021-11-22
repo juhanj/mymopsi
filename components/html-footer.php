9f24f2aaac743ea369e07ed3017c2e7a563f400c
Juhani Järviö <juhani.jarvio@gmail.com>
2021-11-22 13:52:41 +0200

    Update html-footer.php

--- components/html-footer.php
+++ components/html-footer.php
@@ -8,7 +8,7 @@
 	<?php // TODO: Fix this on the front page, looks wrong ?>
 	<section class="right">
 		<a href="<?= WEB_PATH ?>settings.php" class="button light settings-link">
-			Settings
+			<?= $lang->SETTINGS ?>
 			<span class="material-icons">settings</span>
 		</a>
     </section>
