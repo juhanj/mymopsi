c22b5208d6a9c95db8570182bf381d9c4e57e8ac
Juhani Järviö <juhani.jarvio@gmail.com>
2021-11-22 13:52:12 +0200

    Update collection.js
    Overlay stuff

--- js/collection.js
+++ js/collection.js
@@ -1,19 +1,34 @@
 'use strict';
 
+import {Common} from "./modules/export.js";
+
 /* **************************************
 	Functions
  * **************************************/
 
 function openOverlay ( listItem ) {
-	// let image = collectionImages.listIn
+	overlayEditLink.href = `./edit-image.php?id=${listItem.dataset.id}`;
+	overlayMapLocationLink.href = `./map.php?cid=${collectionRUID}&iid=${listItem.dataset.id}`;
+	overlayImageElement.src = `./img/img.php?id=${listItem.dataset.id}&full`;
+
+	let location = "";
+	if ( listItem.dataset.lat ) {
+		location = Common.fGPSDecimalToDMS( {lat:listItem.dataset.lat,lng:listItem.dataset.lng} );
+	}
+	overlayNameTitle.innerHTML = listItem.dataset.name + "<br>" + location;
+
 	overlay.hidden = false;
 	overlay.classList.remove( 'hidden' );
+}
 
-	overlayImageEditLink.href = `./edit-image.php?id=${listItem.dataset.id}`;
-	overlayImageNameTitle.innerText = listItem.dataset.name;
-	overlayImageMapLocationLink.href = `./map.php?cid=${collectionRUID}&iid=${listItem.dataset.id}`;
+function closeOverlay () {
+	overlay.hidden = true;
+	overlay.classList.add( 'hidden' );
 
-	overlayImageElement.src = `./img/img.php?id=${listItem.dataset.id}&full`;
+	overlayEditLink.href = '';
+	overlayNameTitle.innerText = '';
+	overlayMapLocationLink.href = '';
+	overlayImageElement.src = '';
 }
 
 /* **************************************
@@ -28,40 +43,93 @@ headerCollectionNameLink.href = `edit-collection.php?id=${collectionRUID}`;
 
 // Image list elements
 let imageList = document.getElementById( 'imageList' );
+let listItems = imageList.getElementsByTagName("li");
 let imageElements = imageList.querySelectorAll( 'img.img-thumb' );
+// Active element is for keyboard events
+// Defaulted to first, instead of null for less if-else checks
+let activeIndex = -1;
+let activeElement = listItems[0];
+let activeCSSClassName = 'active-list-item';
 
 // Overlay elements:
 let overlay = document.getElementById( 'overlay' );
-let overlayImageEditLink = document.getElementById( 'imageEditLink' );
-let overlayImageNameTitle = document.getElementById( 'imageName' );
-let overlayImageMapLocationLink = document.getElementById( 'imageMapLink' );
+let overlayEditLink = document.getElementById( 'imageEditLink' );
+let overlayNameTitle = document.getElementById( 'imageName' );
+let overlayMapLocationLink = document.getElementById( 'imageMapLink' );
 let overlayClose = document.getElementById( 'closeOverlay' );
 let overlayImageElement = document.getElementById( 'imageFull' );
 
+// If full-sized image fails to load (e.g. file type not supported)
 overlayImageElement.onerror = () => {
 	overlayImageElement.src='./img/mopsi.ico';
 }
 
 // When image is clicked, open fullscreen overlay
 imageList.onclick = (event) => {
-	if ( event.target && event.target.tagName === 'IMG' ) {
+	if ( event.target && event.target.tagName === 'LI' ) {
 		openOverlay( event.target );
 	}
-	else if ( event.target && event.target.tagName === 'LI' ) {
-		openOverlay( event.target.children[0] );
-	}
 }
+
 // Overlay close:
-overlayClose.onclick = () => {
-	//TODO: separate into own function so can use for ESC keyup as well
-	overlay.hidden = true;
-	overlay.classList.add( 'hidden' );
+overlayClose.onclick = closeOverlay;
 
-	overlayImageEditLink.href = '';
-	overlayImageNameTitle.innerText = '';
-	overlayImageMapLocationLink.href = '';
-	overlayImageElement.src = '';
-}
+// Keyboard events, for handling overlay
+document.addEventListener( 'keyup', (event) => {
+	let key = event.key;
 
-//TODO: Add escape keyup eventlistener, for closing overlay (if open)
-// Check for overlay.hidden. No need for separate variable.
+	switch ( key ) {
+		// More to previous element in list of images
+		case 'ArrowLeft':
+			activeElement.classList.remove(activeCSSClassName);
+			--activeIndex;
+
+			// If hit end of list, loop to other end
+			// (in this case, from start to end)
+			if ( activeIndex < 0 ) {
+				activeIndex = listItems.length - 1;
+			}
+
+			activeElement = listItems[activeIndex];
+			activeElement.classList.add(activeCSSClassName);
+
+			if ( !overlay.hidden ) {
+				openOverlay( activeElement );
+			}
+			break;
+
+		// Move to next element in list of images
+		case 'ArrowRight':
+			activeElement.classList.remove(activeCSSClassName);
+			++activeIndex;
+
+			// If hit end of list, loop to other end
+			// (in this case, from end to start)
+			if ( activeIndex >= listItems.length ) {
+				activeIndex = 0;
+			}
+
+			activeElement = listItems[activeIndex];
+			activeElement.classList.add(activeCSSClassName);
+
+			if ( !overlay.hidden ) {
+				openOverlay( activeElement );
+			}
+			break;
+
+		// Open overlay of active element in list (do nothing if overlay open)
+		case ' ': // space
+		case 'Enter':
+			if ( overlay.hidden ) {
+				openOverlay( activeElement );
+			}
+			break;
+
+		// Close overlay (do nothing if overlay closed)
+		case 'Escape':
+			closeOverlay();
+			activeElement.classList.remove(activeCSSClassName);
+			break;
+	}
+	console.log(event);
+} )
