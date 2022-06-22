3700b257414125aeaf1f9c104084298db4849564
Juhani Järviö <juhani.jarvio@gmail.com>
2022-06-22 12:23:11 +0300

    Update html-header.php

--- components/html-header.php
+++ components/html-header.php
@@ -7,7 +7,7 @@
 
 <header class="site-header margins-off" id="site-header">
 	<nav class="breadcrumbs-navigation margins-off">
-		<span><img src="./img/mopsi128.png" alt="MyMopsi" style="height: 1rem"></span>
+		<span><img src="<?= WEB_PATH ?>/img/mopsi128.png" alt="MyMopsi" style="height: 1rem"></span>
 		<span class="separator">•</span>
 		<?php foreach ( $breadcrumbs_navigation as $breadcrumb ) : ?>
 			<a href="<?= $breadcrumb[ 1 ] ?>"><?= $breadcrumb[ 0 ] ?></a>
