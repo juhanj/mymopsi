ab7d3c89212d4cc71d721e93b0c8d66e8212b7e4
Juhani Järviö <juhani.jarvio@gmail.com>
2022-06-22 12:04:10 +0300

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -1,24 +1,5 @@
 <?php declare(strict_types=1);
 require '../components/_start.php';
 /** @var DBConnection $db */
+echo "<style>html{background-color: #111111;color: #ececec;}</style>";
 /*/////////////////////////////////////////////////*/
-
-
-$testImagePath = "./Joensuu/";
-$testThumbPath = "./thumbs/*.JPG";
-
-echo "<p><pre>ImageMagick test | Thumbnail generation</pre>";
-echo "<p><pre>{$testImagePath}</pre>";
-echo "<p><pre>{$testThumbPath}</pre>";
-
-$command = INI[ 'Misc' ][ 'imagemagick' ]
-	. " $testImagePath " // Original image
-	. " -thumbnail 128x128 " // Strip metadata, and size of thumbnail
-	. " -sharpen 0x.5 " // Sharpen image a bit, comes out blurry otherwise
-	. " -gravity center " // Center image for following option
-	. " -extent 128x128 " // Make image square
-	//TODO: transparent background (failed multiple tries) --jj 21-05-16
-	. $testThumbPath; // New thumbnail path
-
-debug( $command );
-//exec( $command, $output, $returnCode );
